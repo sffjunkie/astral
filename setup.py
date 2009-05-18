bc79672ab70bd7657bacf641a6a8091f31be2ae1
Simon Kennedy <sdk@sffjunkie.co.uk>
2009-05-18 19:09:31 +0300

    Updated to match new project name

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-setup(name='celestial',
+setup(name='astral',
     version='0.01',
-    description='Celestial calculations for dawn, sunrise, solar noon, sunset and dusk.',
+    description='Calculations for dawn, sunrise, solar noon, sunset and dusk.',
     author='Simon Kennedy',
-    author_email='celestial@sffjunkie.co.uk',
-    url="http://www.sffjunkie.co.uk/python-celestial.html",
+    author_email='astral@sffjunkie.co.uk',
+    url="http://www.sffjunkie.co.uk/python-astral.html",
     license='MIT',
-    py_modules=['celestial'],
+    py_modules=['astral'],
     install_requires=['pytz']
 )
 
