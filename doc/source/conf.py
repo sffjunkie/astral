caa980035632a5c651c4d0aa6d3db64cfc67edb4
Simon Kennedy <sdk@sffjunkie.co.uk>
2009-05-26 07:07:36 +0300

    Changed directories _static and _templates to static and templates respectively.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.abspath('../..'))
 extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -120,7 +120,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
