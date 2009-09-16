76d1dfd931ded8daad81aced7f4c6d8637165e8d
Simon Kennedy <sdk@sffjunkie.co.uk>
2009-09-16 10:09:51 +0300

    Changed output directory to 'doc'

--- doc/make.bat
+++ doc/make.bat
@@ -33,9 +33,9 @@ if "%1" == "clean" (
 )
 
 if "%1" == "html" (
-	%SPHINXBUILD% -b html %ALLSPHINXOPTS% ../build/html
+	%SPHINXBUILD% -b html %ALLSPHINXOPTS% ../build/doc
 	echo.
-	echo.Build finished. The HTML pages are in ../build/html.
+	echo.Build finished. The HTML pages are in ../build/doc.
 	goto end
 )
 
