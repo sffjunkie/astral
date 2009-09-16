34ab183d397921b7309935375e0e42762872d16b
Simon Kennedy <sdk@sffjunkie.co.uk>
2009-09-16 11:27:49 +0300

    Added new tests

--- test/testAstral.py
+++ test/testAstral.py
@@ -37,21 +37,10 @@ def testAstral():
     print('Latitude: %.02f; Longitude: %.02f\n' % (city.latitude, city.longitude))
     
     sun = city.sun()
-    print('Dawn:     %s' % str(sun['dawn']))
-    print('Sunrise:  %s' % str(sun['sunrise']))
-    print('Noon:     %s' % str(sun['noon']))
-    print('Sunset:   %s' % str(sun['sunset']))
-    print('Dusk:     %s' % str(sun['dusk']))
-    
-    rahukaalam = city.rahukaalam()
-    print('\nRahukaalam')
-    print('Start:   %s' % str(rahukaalam['start']))
-    print('End:     %s' % str(rahukaalam['end']))
-    
     sunrise = city.sunrise(local=True)
-    print('\nSunrise: %s' % str(sunrise))
-    
     assert sunrise == sun['sunrise']
+
+    rahukaalam = city.rahukaalam()
 
 def testElevation():
     city_name = 'Jubail'
