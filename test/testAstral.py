8925cc2feb5a0eb148960257b6b9d04959f426d9
Simon Kennedy <python@sffjunkie.co.uk>
2011-02-27 08:39:11 +0000

    Added testCityDB.py and moved testAllCities from testAstral.py

--- test/testAstral.py
+++ test/testAstral.py
@@ -98,14 +98,6 @@ def testAdelaide():
 def testMoon():
     dd = Astral()
     dd.moon(datetime.date(2011,02,24))
-
-def testAllCities():
-    dd = Astral()
-    cities = dd.cities.keys()
-    cities.sort()
-    
-    for city_name in cities:
-        city = dd[city_name]
     
 if __name__ == "__main__":
     testAstral()
@@ -115,4 +107,3 @@ if __name__ == "__main__":
     testMultiCountry()
     testAdelaide()
     testMoon()
-    testAllCities()
---
+++ test/testCityDB.py
@@ -0,0 +1,23 @@
+from nose.tools import raises
+
+from astral import CityDB
+
+def testGroup():
+    db = CityDB()
+    e = db.europe
+    
+@raises(AttributeError)    
+def testUnknownGroup():
+    db = CityDB()
+    e = db.wallyland
+
+def testAllCities():
+    db = CityDB()
+    cities = db.cities
+    cities.sort()
+    
+    for city_name in cities:
+        city = db[city_name]
+
+if __name__ == "__main__":
+    testAllCities()
