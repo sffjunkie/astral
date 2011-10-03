9413e7bced0b138b52836fb196a31447ab234064
Simon Kennedy <python@sffjunkie.co.uk>
2011-10-03 10:39:49 +0100

    Fixed relative paths

--- astral/doc/make.bat
+++ astral/doc/make.bat
@@ -33,35 +33,35 @@ if "%1" == "clean" (
 )
 
 if "%1" == "html" (
-	%SPHINXBUILD% -b html %ALLSPHINXOPTS% ../../doc
+	%SPHINXBUILD% -b html %ALLSPHINXOPTS% ../../build/doc
 	echo.
-	echo.Build finished. The HTML pages are in ../../doc.
+	echo.Build finished. The HTML pages are in ../../build/doc.
 	goto end
 )
 
 if "%1" == "dirhtml" (
-	%SPHINXBUILD% -b dirhtml %ALLSPHINXOPTS% ../build/dirhtml
+	%SPHINXBUILD% -b dirhtml %ALLSPHINXOPTS% ../../build/dirhtml
 	echo.
-	echo.Build finished. The HTML pages are in ../build/dirhtml.
+	echo.Build finished. The HTML pages are in ../../build/dirhtml.
 	goto end
 )
 
 if "%1" == "pickle" (
-	%SPHINXBUILD% -b pickle %ALLSPHINXOPTS% ../build/pickle
+	%SPHINXBUILD% -b pickle %ALLSPHINXOPTS% ../../build/pickle
 	echo.
 	echo.Build finished; now you can process the pickle files.
 	goto end
 )
 
 if "%1" == "json" (
-	%SPHINXBUILD% -b json %ALLSPHINXOPTS% ../build/json
+	%SPHINXBUILD% -b json %ALLSPHINXOPTS% ../../build/json
 	echo.
 	echo.Build finished; now you can process the JSON files.
 	goto end
 )
 
 if "%1" == "htmlhelp" (
-	%SPHINXBUILD% -b htmlhelp %ALLSPHINXOPTS% ../build/htmlhelp
+	%SPHINXBUILD% -b htmlhelp %ALLSPHINXOPTS% ../../build/htmlhelp
 	echo.
 	echo.Build finished; now you can run HTML Help Workshop with the ^
 .hhp project file in ../build/htmlhelp.
@@ -69,43 +69,43 @@ if "%1" == "htmlhelp" (
 )
 
 if "%1" == "qthelp" (
-	%SPHINXBUILD% -b qthelp %ALLSPHINXOPTS% ../build/qthelp
+	%SPHINXBUILD% -b qthelp %ALLSPHINXOPTS% ../../build/qthelp
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in ../build/qthelp, like this:
-	echo.^> qcollectiongenerator ../build\qthelp\Astral.qhcp
+	echo.^> qcollectiongenerator ../../build\qthelp\Astral.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile ../build\qthelp\Astral.ghc
+	echo.^> assistant -collectionFile ../../build\qthelp\Astral.ghc
 	goto end
 )
 
 if "%1" == "latex" (
-	%SPHINXBUILD% -b latex %ALLSPHINXOPTS% ../build/latex
+	%SPHINXBUILD% -b latex %ALLSPHINXOPTS% ../../build/latex
 	echo.
-	echo.Build finished; the LaTeX files are in ../build/latex.
+	echo.Build finished; the LaTeX files are in ../../build/latex.
 	goto end
 )
 
 if "%1" == "changes" (
-	%SPHINXBUILD% -b changes %ALLSPHINXOPTS% ../build/changes
+	%SPHINXBUILD% -b changes %ALLSPHINXOPTS% ../../build/changes
 	echo.
 	echo.The overview file is in ../build/changes.
 	goto end
 )
 
 if "%1" == "linkcheck" (
-	%SPHINXBUILD% -b linkcheck %ALLSPHINXOPTS% ../build/linkcheck
+	%SPHINXBUILD% -b linkcheck %ALLSPHINXOPTS% ../../build/linkcheck
 	echo.
 	echo.Link check complete; look for any errors in the above output ^
-or in ../build/linkcheck/output.txt.
+or in ../../build/linkcheck/output.txt.
 	goto end
 )
 
 if "%1" == "doctest" (
-	%SPHINXBUILD% -b doctest %ALLSPHINXOPTS% ../build/doctest
+	%SPHINXBUILD% -b doctest %ALLSPHINXOPTS% ../../build/doctest
 	echo.
 	echo.Testing of doctests in the sources finished, look at the ^
-results in ../build/doctest/output.txt.
+results in ../../build/doctest/output.txt.
 	goto end
 )
 
