416cf68f96de8b481cff0c878a4748004b66fa02
Simon Kennedy <code@sffjunkie.co.uk>
2013-02-01 10:45:41 +0000

    Added a 'pass' statement to allow adding a debugger breakpoint

--- astral/test/test_GoogleGeocoder.py
+++ astral/test/test_GoogleGeocoder.py
@@ -3,7 +3,7 @@ from astral import GoogleGeocoder
 def test_GoogleLocator():
     locator = GoogleGeocoder()
     l = locator['Eiffel Tower']
-    
+    pass
 
 if __name__ == '__main__':
     test_GoogleLocator()
