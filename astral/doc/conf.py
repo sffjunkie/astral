6c7be99b9bc4388031719c792a2935fb7518bbbf
Simon Kennedy <code@sffjunkie.co.uk>
2013-02-04 18:31:30 +0000

    Only set custom favicon when not building on readthedocs

--- astral/doc/conf.py
+++ astral/doc/conf.py
@@ -150,7 +150,8 @@ html_logo = os.path.join('_static', 'earth_sun.png')
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = os.path.join('static', 'favicon.png')
+if on_rtd:
+    html_favicon = os.path.join('_static', 'favicon.png')
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
