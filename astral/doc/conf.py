49bf8712337092ffd2e148ef1f3834f55ac26fba
Simon Kennedy <code@sffjunkie.co.uk>
2013-02-04 18:28:29 +0000

    Added mock'ing of pytz module. Taken from the readthedocs FAQ

--- astral/doc/conf.py
+++ astral/doc/conf.py
@@ -13,6 +13,28 @@
 
 import sys, os
 
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['pytz']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+    
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 doc_root = os.path.abspath(os.path.dirname(__file__))
