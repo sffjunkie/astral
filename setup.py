232dcd5262b421551420a526d696ed91e97654e3
Simon Kennedy <code@sffjunkie.co.uk>
2014-09-21 07:47:45 +0100

    Added `tox` as the test command

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
 # Copyright 2009-2014, Simon Kennedy, sffjunkie+code@gmail.com
 
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+        
+    def run_tests(self):
+        import tox
+        errcode = tox.cmdline(self.test_args)
+        sys.exit(errcode)
+
+        
 setup(name='astral',
     version='0.7.4',
     description='Calculations for the position of the sun and moon.',
@@ -16,4 +31,7 @@ setup(name='astral',
     package_dir={'': 'src'},
     py_modules=['astral'],
     install_requires=['pytz'],
+    
+    tests_require=['tox'],
+    cmdclass = {'test': Tox},
 )
