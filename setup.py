80b775ea206ac01ae4f12cc78a91458341d12627
Simon Kennedy <code@sffjunkie.co.uk>
2014-09-21 11:43:09 +0100

    Home page updated

--- setup.py
+++ setup.py
@@ -18,20 +18,20 @@ class Tox(TestCommand):
 
         
 setup(name='astral',
-    version='0.7.4',
-    description='Calculations for the position of the sun and moon.',
-    long_description="""Sun calculations for dawn, sunrise, solar noon,
-    sunset, dusk, solar elevation, solar azimuth and rahukaalam.
-    Moon calculation for phase.
-    """,
-    author='Simon Kennedy',
-    author_email='sffjunkie+code@gmail.com',
-    url="http://www.sffjunkie.co.uk/python-astral.html",
-    license='Apache-2.0',
-    package_dir={'': 'src'},
-    py_modules=['astral'],
-    install_requires=['pytz'],
+      version='0.7.4',
+      description='Calculations for the position of the sun and moon.',
+      long_description="""Sun calculations for dawn, sunrise, solar noon,
+      sunset, dusk, solar elevation, solar azimuth and rahukaalam.
+      Moon calculation for phase.
+      """,
+      author='Simon Kennedy',
+      author_email='sffjunkie+code@gmail.com',
+      url="https://launchpad.net/astral",
+      license='Apache-2.0',
+      package_dir={'': 'src'},
+      py_modules=['astral'],
+      install_requires=['pytz'],
     
-    tests_require=['tox'],
-    cmdclass = {'test': Tox},
+      tests_require=['tox'],
+      cmdclass = {'test': Tox},
 )
