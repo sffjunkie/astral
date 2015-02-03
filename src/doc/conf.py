6f014ae45d06104e1c7df579cd3fe640d0a7d23b
Simon Kennedy <sffjunkie+code@gmail.com>
2015-02-03 11:39:55 +0000

    Updated version number

--- src/doc/conf.py
+++ src/doc/conf.py
@@ -24,8 +24,8 @@ if MOCK_MODULES and on_rtd:
 project = 'Astral'
 author = 'Simon Kennedy'
 copyright = '2009-2014, %s' % author
-version = '0.7'
-release = '0.7.5'
+version = '0.8'
+release = '0.8'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
