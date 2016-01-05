f16ada884d38137f4652514469e584c82bf91f06
Simon Kennedy <sffjunkie+code@gmail.com>
2016-01-05 16:59:46 +0000

    Changed to use float_almost_equal

--- src/test/test_Astral.py
+++ src/test/test_Astral.py
@@ -11,6 +11,10 @@ import datetime
 from astral import Astral
 
 
+def float_almost_equal(value1, value2, diff=0.5):
+    return abs(value1 - value2) <= diff
+
+
 def test_AstralBadLocationName():
     with raises(KeyError):
         dd = Astral()
@@ -48,7 +52,7 @@ def test_Astral_SolarElevation():
     dt = datetime.datetime(2015, 2, 3, 9, 0, 0, tzinfo=pytz.UTC)
 
     elevation = dd.solar_elevation(dt, 51.5, -0.12)
-    assert abs(elevation - 9.97) < 0.1
+    assert float_almost_equal(elevation, 9.97, 0.1)
 
 
 def test_Astral_SolarAzimuth():
@@ -56,7 +60,7 @@ def test_Astral_SolarAzimuth():
     dt = datetime.datetime(2015, 2, 3, 9, 0, 0, tzinfo=pytz.UTC)
 
     azimuth = dd.solar_azimuth(dt, 51.5, -0.12)
-    assert abs(azimuth - 133.162) < 0.1
+    assert float_almost_equal(azimuth, 133.162, 0.1)
 
 
 def test_Astral_SolarElevationWithTimezone():
@@ -65,7 +69,7 @@ def test_Astral_SolarElevationWithTimezone():
 
     dt = datetime.datetime(2015, 2, 4, 9, 0, 0, tzinfo=location.tz)
     elevation = dd.solar_elevation(dt, location.latitude, location.longitude)
-    assert abs(elevation - 28.118) < 0.1
+    assert float_almost_equal(elevation, 28.118, 0.1)
 
 
 def test_Astral_SolarAzimuthWithTimezone():
@@ -74,7 +78,7 @@ def test_Astral_SolarAzimuthWithTimezone():
 
     dt = datetime.datetime(2015, 2, 4, 9, 0, 0, tzinfo=location.tz)
     azimuth = dd.solar_azimuth(dt, location.latitude, location.longitude)
-    assert abs(azimuth - 129.02) < 0.1
+    assert float_almost_equal(azimuth, 129.02, 0.1)
 
 
 def test_Elevation():
@@ -89,17 +93,17 @@ def test_Astral_JulianDay():
     
     dt = datetime.date(2015, 1, 1)
     jd = a._julianday(dt)
-    assert  abs(jd - 2457023.5) < 0.1
+    assert float_almost_equal(jd, 2457023.5, 0.1)
     
     dt = datetime.date(2015, 2, 9)
     jd = a._julianday(dt)
-    assert  abs(jd - 2457062.5) < 0.1
+    assert float_almost_equal(jd, 2457062.5, 0.1)
     
     dt = datetime.date(2000, 8, 12)
     jd = a._julianday(dt)
-    assert  abs(jd - 2451768.5) < 0.1
+    assert float_almost_equal(jd, 2451768.5, 0.1)
     
     dt = datetime.date(1632, 8, 12)
     jd = a._julianday(dt)
-    assert  abs(jd - 2317359.5) < 0.1
+    assert float_almost_equal(jd, 2317359.5, 0.1)
     
