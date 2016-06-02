15285e8ebe1efb73d0a8179bd81a7ffca2d75ef0
Simon Kennedy <sffjunkie+code@gmail.com>
2016-06-02 16:31:02 +0100

    Added test for computing dawn time for Ghent at a depression of 18 degrees. This should raise an exception as the sun never reaches that depression. Later we'll define an exception subclass for this error.

---
+++ src/test/test_Ghent.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest 
+import astral
+
+
+def test_GhentDawn_NeverReachesDepression():
+    with pytest.raises(astral.AstralError):
+        a = astral.Astral(astral.GoogleGeocoder)
+        l = a['Ghent,Belgium']
+        l.solar_depression = 18
+        l.dawn_utc(local=True)
