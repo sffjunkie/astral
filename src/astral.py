e0ce1b2ad8d4f109b15cdbb1606dcb96a3241495
Simon Kennedy <sffjunkie+code@gmail.com>
2016-10-26 09:10:16 +0100

    Changed version number to 1.3

--- src/astral.py
+++ src/astral.py
@@ -95,7 +95,7 @@ __all__ = ['Astral', 'Location',
            'AstralGeocoder', 'GoogleGeocoder',
            'AstralError']
 
-__version__ = "1.2"
+__version__ = "1.3"
 __author__ = "Simon Kennedy <sffjunkie+code@gmail.com>"
 
 SUN_RISING = 1
