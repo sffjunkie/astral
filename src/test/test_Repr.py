aa900de110f1cca17c073a9aacb23192ad55da03
Simon Kennedy <sffjunkie+code@gmail.com>
2017-12-07 10:29:53 +0000

    Added test for a Location's repr

---
+++ src/test/test_Repr.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from astral import Astral, Location
+
+def test_LocationRepr():
+    l = Location(('London', 'England', 51.68, -0.05, 'Europe/London', 3))
+    assert l.__repr__() == 'London/England, tz=Europe/London, lat=51.68, lon=-0.05'
