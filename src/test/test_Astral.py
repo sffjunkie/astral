3e2449b34093ffe7938559eec0a03a12eada292f
Simon Kennedy <sffjunkie+code@gmail.com>
2017-12-07 16:01:08 +0000

    - Added parametrized tests for invalid values to _utc functions - Changed pytest import

--- src/test/test_Astral.py
+++ src/test/test_Astral.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(0,
                 os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from pytest import raises
+import pytest
 
 import pytz
 import datetime
@@ -17,7 +17,7 @@ def float_almost_equal(value1, value2, diff=0.5):
 
 
 def test_AstralBadLocationName():
-    with raises(KeyError):
+    with pytest.raises(KeyError):
         dd = Astral()
         _c = dd['wally']
 
@@ -29,7 +29,7 @@ def test_AstralLocationName():
 
 
 def test_AstralAssign():
-    with raises(TypeError):
+    with pytest.raises(TypeError):
         dd = Astral()
         dd['London'] = 'wally'
 
@@ -102,43 +102,43 @@ def test_Astral_SolarAzimuthWithTimezone():
 
 def test_Astral_JulianDay_Date():
     a = Astral()
-    
+
     dt = datetime.date(2015, 1, 1)
     jd = a._julianday(dt)
     assert float_almost_equal(jd, 2457023.5, 0.1)
-    
+
     dt = datetime.date(2015, 2, 9)
     jd = a._julianday(dt)
     assert float_almost_equal(jd, 2457062.5, 0.1)
-    
+
     dt = datetime.date(2000, 8, 12)
     jd = a._julianday(dt)
     assert float_almost_equal(jd, 2451768.5, 0.1)
-    
+
     dt = datetime.date(1632, 8, 12)
     jd = a._julianday(dt)
     assert float_almost_equal(jd, 2317359.5, 0.1)
-    
-    
+
+
 def test_Astral_JulianDay_DateTime():
     a = Astral()
-    
+
     dt = datetime.datetime(2015, 1, 1, 1, 36, 0)
     jd = a._julianday(dt)
     assert float_almost_equal(jd, 2457023.57, 0.1)
-    
+
     dt = datetime.datetime(2015, 1, 1, 15, 12, 0)
     jd = a._julianday(dt)
     assert float_almost_equal(jd, 2457024.13, 0.1)
-    
-    
+
+
 def test_Astral_JulianDay_DateTimeZone():
     a = Astral()
-    
+
     dt = datetime.datetime(2015, 1, 1, 1, 36, 0)
     jd = a._julianday(dt, 1)
     assert float_almost_equal(jd, 2457023.51, 0.1)
-    
+
     dt = datetime.datetime(2015, 10, 10, 1, 36, 0)
     jd = a._julianday(dt, 5)
     assert float_almost_equal(jd, 2457305.36, 0.1)
@@ -146,7 +146,7 @@ def test_Astral_JulianDay_DateTimeZone():
 
 def test_GeomMeanLongSun():
     a = Astral()
-    
+
     dt = datetime.datetime(2015, 10, 10, 1, 36, 0)
     jc = a._jday_to_jcentury(a._julianday(dt, 5))
     geom = a._geom_mean_long_sun(jc)
@@ -160,7 +160,7 @@ def test_GeomMeanLongSun():
 
 def test_GeomMeanAnomalySun():
     a = Astral()
-    
+
     dt = datetime.date(2015, 10, 10)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     geom = a._geom_mean_anomaly_sun(jc)
@@ -169,7 +169,7 @@ def test_GeomMeanAnomalySun():
 
 def test_EccentrilocationEarthOrbit():
     a = Astral()
-    
+
     dt = datetime.date(2015, 10, 10)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     ecc = a._eccentrilocation_earth_orbit(jc)
@@ -178,7 +178,7 @@ def test_EccentrilocationEarthOrbit():
 
 def test_SunEqOfCenter():
     a = Astral()
-    
+
     dt = datetime.date(2015, 10, 10)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     eoc = a._sun_eq_of_center(jc)
@@ -187,7 +187,7 @@ def test_SunEqOfCenter():
 
 def test_SunTrueLong():
     a = Astral()
-    
+
     dt = datetime.date(2015, 10, 10)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     true_long = a._sun_true_long(jc)
@@ -196,7 +196,7 @@ def test_SunTrueLong():
 
 def test_SunTrueAnomoly():
     a = Astral()
-    
+
     dt = datetime.date(2015, 10, 10)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     true_long = a._sun_true_anomoly(jc)
@@ -205,7 +205,7 @@ def test_SunTrueAnomoly():
 
 def test_SunRadVector():
     a = Astral()
-    
+
     dt = datetime.date(2015, 10, 10)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     rad_vector = a._sun_rad_vector(jc)
@@ -214,7 +214,7 @@ def test_SunRadVector():
 
 def test_SunApparentLong():
     a = Astral()
-    
+
     dt = datetime.date(2015, 10, 10)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     apparent_long = a._sun_apparent_long(jc)
@@ -223,7 +223,7 @@ def test_SunApparentLong():
 
 def test_MeanObliquityOfEcliptic():
     a = Astral()
-    
+
     dt = datetime.date(2015, 10, 10)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     mean_obliquity = a._mean_obliquity_of_ecliptic(jc)
@@ -232,16 +232,16 @@ def test_MeanObliquityOfEcliptic():
 
 def test_ObliquityCorrection():
     a = Astral()
-    
+
     dt = datetime.datetime(2015, 10, 10, 1, 36, 0)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     obliquity_correction = a._obliquity_correction(jc)
     assert float_almost_equal(obliquity_correction, 23.43468009, 0.0001)
-    
-    
+
+
 def test_SunRightAscension():
     a = Astral()
-    
+
     dt = datetime.date(2015, 10, 10)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     rt_ascension = a._sun_rt_ascension(jc)
@@ -250,7 +250,7 @@ def test_SunRightAscension():
 
 def test_SunDeclination():
     a = Astral()
-    
+
     dt = datetime.datetime(2015, 10, 10, 1, 36, 0)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     declination = a._sun_declination(jc)
@@ -259,7 +259,7 @@ def test_SunDeclination():
 
 def test_VarY():
     a = Astral()
-    
+
     dt = datetime.datetime(2015, 10, 10, 0, 54, 0)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     y = a._var_y(jc)
@@ -268,12 +268,12 @@ def test_VarY():
 
 def test_EquationOfTime():
     a = Astral()
-    
+
     dt = datetime.datetime(2015, 10, 10, 0, 54, 0)
     jc = a._jday_to_jcentury(a._julianday(dt, -4))
     etime = a._eq_of_time(jc)
     assert float_almost_equal(etime, 12.84030157, 0.0001)
-    
+
     dt = datetime.datetime(2017, 1, 1, 2, 0, 0)
     jc = a._jday_to_jcentury(a._julianday(dt, 2))
     etime = a._eq_of_time(jc)
@@ -282,9 +282,37 @@ def test_EquationOfTime():
 
 def test_SunriseHourAngle():
     a = Astral()
-    
+
     dt = datetime.date(2015, 1, 1)
     jc = a._jday_to_jcentury(a._julianday(dt, 1))
     declination = a._sun_declination(jc)
     ha = math.degrees(a._hour_angle(52.169, declination, 90.833))
     assert float_almost_equal(ha, 58.6102679, 0.1)
+
+@pytest.mark.parametrize("test", ["dawn_utc", "sunrise_utc", "sunset_utc", "dusk_utc"])
+def test_UTC_BadDate(test):
+    a = Astral()
+    func = getattr(a, test)
+
+    with pytest.raises(AttributeError):
+        l = func('s', 1, -0.5)
+
+
+@pytest.mark.parametrize("test", ["dawn_utc", "sunrise_utc", "sunset_utc", "dusk_utc"])
+def test_UTC_BadLatitude(test):
+    a = Astral()
+    func = getattr(a, test)
+    dt = datetime.date(2015, 1, 1)
+
+    with pytest.raises(TypeError):
+        l = func(dt, 1, 'a')
+
+
+@pytest.mark.parametrize("test", ["dawn_utc", "sunrise_utc", "sunset_utc", "dusk_utc"])
+def test_UTC_BadLongitude(test):
+    a = Astral()
+    func = getattr(a, test)
+    dt = datetime.date(2015, 1, 1)
+
+    with pytest.raises(TypeError):
+        l = func(dt, 'a', 1)
