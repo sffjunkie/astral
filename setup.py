0994c5178c6f43fdd6feaff10a3f6e3b6fa373b4
Simon Kennedy <sffjunkie+code@gmail.com>
2018-01-17 17:57:08 +0000

    Removed code that has a better alternative (pytest-runner)

--- setup.py
+++ setup.py
@@ -1,44 +1,17 @@
 # Copyright 2009-2017, Simon Kennedy, sffjunkie+code@gmail.com
 
+import io
 import os
-import sys
 from setuptools import setup
 
-PACKAGE_DIR = 'src'
-
-sys.path.insert(0, PACKAGE_DIR)
-
-def dev_dir():
-    dev_home = os.environ.get('DEV_HOME', None)
-    if not dev_home:
-        return None
-
-    return os.path.join(dev_home, 'projects')
-
-cmd_class = {}
-tests_require = []
-
-dd = dev_dir()
-if dd:
-    sys.path.insert(0, dd)
-
-try:
-    import common.test
-    cmd_class['test'] = common.test.TestCommand
-    tests_require.extend(common.test.requires)
-except ImportError:
-    print('Unable to import test command. Skipping. Try running tool from command line e.g. tox')
-    pass
-
-#try:
-#    import common.clean
-#    cmd_class['clean'] = common.clean.CleanCommand
-#except ImportError:
-#    pass
+def read_contents(*names, **kwargs):
+    return io.open(
+        os.path.join(*names),
+        encoding=kwargs.get("encoding", "utf8")
+    ).read()
 
 description = 'Calculations for the position of the sun and moon.'
 try:
-    from common.setup_funcs import read_contents
     long_description = read_contents(os.path.dirname(__file__), 'README.md')
 except ImportError:
     long_description = description
@@ -57,10 +30,8 @@ setup(name='astral',
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
       ],
-      package_dir={'': PACKAGE_DIR},
+      package_dir={'': 'src'},
       py_modules=['astral'],
       install_requires=['pytz'],
-
-      cmdclass = cmd_class,
-      tests_require=tests_require,
+      tests_require=['pytest-runner'],
 )
