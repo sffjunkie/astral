11e8620c34fe8e4e8107a16ebfd8aa0220ecdb04
Simon Kennedy <sffjunkie+code@gmail.com>
2018-02-23 16:09:15 +0000

    Skipt tests if .api_key file found or not.

--- src/test/test_GoogleGeocoder.py
+++ src/test/test_GoogleGeocoder.py
@@ -4,27 +4,30 @@ import pytest
 
 from astral import GoogleGeocoder
 
-@pytest.mark.webtest
-def test_GoogleLocator():
-    locator = GoogleGeocoder()
-    l = locator['Eiffel Tower']
-    assert l is not None
-
 def read_contents(*names, **kwargs):
     return io.open(
         os.path.join(*names),
         encoding=kwargs.get("encoding", "utf8")
     ).read()
 
-api_key = read_contents(os.path.dirname(__file__), '.api_key')
+try:
+    api_key = read_contents(os.path.dirname(__file__), '.api_key').strip()
+except:
+    api_key = ''
+
 @pytest.mark.webtest
+@pytest.mark.skipif(api_key != '', reason="api key file found")
+def test_GoogleLocator():
+    locator = GoogleGeocoder()
+    l = locator['Eiffel Tower']
+    assert l is not None
+
+@pytest.mark.webtest
+@pytest.mark.skipif(api_key == '', reason="api key file not found")
 def test_GoogleLocator_WithAPIKey():
-    if api_key:
-        locator = GoogleGeocoder(api_key=api_key)
-        l = locator['Eiffel Tower']
-        assert l is not None
-    else:
-        raise AssertionError('api_key not found')
+    locator = GoogleGeocoder(api_key=api_key)
+    l = locator['Eiffel Tower']
+    assert l is not None
 
 
 if __name__ == '__main__':
