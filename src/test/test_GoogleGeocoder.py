48d0372958c378eee34da34bc9a1ce78cabe9aa0
Simon Kennedy <sffjunkie+code@gmail.com>
2018-02-27 10:32:13 +0000

    Removed `if __main__ ...`

--- src/test/test_GoogleGeocoder.py
+++ src/test/test_GoogleGeocoder.py
@@ -28,7 +28,3 @@ def test_GoogleLocator_WithAPIKey():
     locator = GoogleGeocoder(api_key=api_key)
     l = locator['Eiffel Tower']
     assert l is not None
-
-
-if __name__ == '__main__':
-    test_GoogleLocator()
