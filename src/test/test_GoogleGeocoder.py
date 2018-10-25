9e580e3935ff23a6915877c22ea9fea651020c82
Simon Kennedy <sffjunkie+code@gmail.com>
2018-10-25 12:08:21 +0100

    Added our Google api key as an encrypted travis ci value.

--- src/test/test_GoogleGeocoder.py
+++ src/test/test_GoogleGeocoder.py
@@ -1,5 +1,5 @@
 import io
-import os.path
+import os
 import pytest
 
 from astral import GoogleGeocoder
@@ -10,10 +10,14 @@ def read_contents(*names, **kwargs):
         encoding=kwargs.get("encoding", "utf8")
     ).read()
 
-try:
-    api_key = read_contents(os.path.dirname(__file__), '.api_key').strip()
-except IOError as exc:
-    raise ValueError("Google now requires an API key to be provided") from exc
+# 1st try to use the API_KEY environment variable that we encrypted
+# in our .travis.yml
+api_key = os.environ.get("API_KEY", None)
+if not api_key:
+	try:
+	    api_key = read_contents(os.path.dirname(__file__), '.api_key').strip()
+	except IOError as exc:
+	    raise ValueError("Google now requires an API key to be provided") from exc
 
 @pytest.mark.webtest
 def test_GoogleLocator_WithAPIKey():
