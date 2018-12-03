68d348269e8fcbe985a79a6b066a06fb1842ce02
Simon Kennedy <sffjunkie+code@gmail.com>
2018-12-03 11:59:19 +0000

    Added command line interface to return 'sun' values as json

--- src/astral.py
+++ src/astral.py
@@ -2609,3 +2609,35 @@ class Astral(object):
         elong = round(elong)
         moon = ((elong + 6.43) / 360) * 28
         return moon
+
+
+if __name__ == "__main__":
+    import argparse
+
+    options = argparse.ArgumentParser()
+    options.add_argument("-n", "--name", dest="name", help="Location name (free-form text)")
+    options.add_argument("--lat", dest="latitude", type=float, help="Location latitude (float)")
+    options.add_argument("--lng", dest="longitude", type=float, help="Location longitude (float)")
+    options.add_argument("-d", "--date", dest="date", help="Date to compute times for (yyyy-mm-dd)")
+    options.add_argument("-t", "--tzname", help="Timezone name")
+    args = options.parse_args()
+
+    loc = Location((args.name, None, args.latitude, args.longitude, args.tzname, 0))
+
+    kwargs = {}
+    if args.date is not None:
+        try:
+            kwargs["date"] = datetime.datetime.strptime(args.date, "%Y-%m-%d").date()
+        except:  # pylint: disable=E0722
+            kwargs["date"] = datetime.date.today()
+
+    if args.tzname is None:
+        kwargs["local"] = False
+
+    sun = loc.sun(**kwargs)
+
+    sun_as_str = {}
+    for key, value in sun.items():
+        sun_as_str[key] = sun[key].strftime("%Y-%m-%dT%H:%M:%S")
+
+    print(json.dumps(sun_as_str))
