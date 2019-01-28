b8ddb5e66d57f16b38c9637b7427489d679f42ab
Simon Kennedy <sffjunkie+code@gmail.com>
2019-01-28 14:23:59 +0000

    Updated version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except:
     long_description = description
 
 setup(name='astral',
-    version='1.8',
+    version='1.9',
     description=description,
     long_description=long_description,
     author='Simon Kennedy',
