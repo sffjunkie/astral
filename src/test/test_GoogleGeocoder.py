dd3d6ad2c8b3d4184342fcbca25f41ae6edf3ed5
Robert Schütz <rschuetz17@gmail.com>
2019-02-05 11:39:35 +0100

    Read .api_key inside test function (#26)
    Otherwise, the `.api_key` file is required even if the `test_GoogleLocator_WithAPIKey` test is not run.

--- src/test/test_GoogleGeocoder.py
+++ src/test/test_GoogleGeocoder.py
@@ -10,17 +10,17 @@ def read_contents(*names, **kwargs):
         encoding=kwargs.get("encoding", "utf8")
     ).read()
 
-# 1st try to use the API_KEY environment variable that we encrypted
-# in our .travis.yml
-api_key = os.environ.get("API_KEY", None)
-if not api_key:
-	try:
-	    api_key = read_contents(os.path.dirname(__file__), '.api_key').strip()
-	except IOError as exc:
-	    raise ValueError("Google now requires an API key to be provided")
-
 @pytest.mark.webtest
 def test_GoogleLocator_WithAPIKey():
+    # 1st try to use the API_KEY environment variable that we encrypted
+    # in our .travis.yml
+    api_key = os.environ.get("API_KEY", None)
+    if not api_key:
+        try:
+            api_key = read_contents(os.path.dirname(__file__), '.api_key').strip()
+        except IOError as exc:
+            raise ValueError("Google now requires an API key to be provided")
+    
     locator = GoogleGeocoder(api_key=api_key)
     l = locator['Eiffel Tower']
     assert l is not None
