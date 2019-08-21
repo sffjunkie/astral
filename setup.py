c6a6256ae974926b73a7a1098c1f1a5b52c2ec1e
Simon Kennedy <sffjunkie+code@gmail.com>
2019-08-21 12:02:44 +0100

    Moved setup options to setup.cfg

--- setup.py
+++ setup.py
@@ -17,30 +17,4 @@ try:
 except:
     long_description = description
 
-setup(
-    name="astral",
-    version="2.0.0a",
-    description=description,
-    long_description=long_description,
-    author="Simon Kennedy",
-    author_email="sffjunkie+code@gmail.com",
-    url="https://github.com/sffjunkie/astral",
-    project_urls={
-        "Documentation": "https://astral.readthedocs.io/en/stable/index.html",
-        "Source": "https://github.com/sffjunkie/astral",
-        "Issues": "https://github.com/sffjunkie/astral/issues",
-    },
-    keywords="sun moon sunrise sunset dawn dusk",
-    license="Apache-2.0",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-    ],
-    package_dir={"": "src"},
-    packages=["astral"],
-    install_requires=["pytz", 'dataclasses;python_version=="3.6"'],
-    tests_require=["pytest-runner"],
-    zip_safe=True,
-)
+setup(long_description=long_description)
