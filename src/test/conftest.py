9b58f42e36b187be0892c968b3c1dba967398d53
Simon Kennedy <sffjunkie+code@gmail.com>
2020-01-23 12:34:14 +0000

    Data now moved back into test module

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -40,25 +40,3 @@ def riyadh_info() -> LocationInfo:
 @pytest.fixture
 def riyadh(riyadh_info) -> Location:
     return Location(riyadh_info)
-
-
-@pytest.fixture
-def julian_day_test_data():
-    return [
-        (datetime.date(2012, 1, 1), 2455927.5),
-        (datetime.date(2013, 1, 1), 2456293.5),
-        (datetime.date(2013, 6, 1), 2456444.5),
-        (datetime.date(1867, 2, 1), 2402998.5),
-        (datetime.date(3200, 11, 14), 2890153.5),
-    ]
-
-
-@pytest.fixture
-def julian_century_test_data():
-    return [
-        (2455927.5, 0.119986311),
-        (2456293.5, 0.130006845),
-        (2456444.5, 0.134140999),
-        (2402998.5, -1.329130732),
-        (2890153.5, 12.00844627),
-    ]
