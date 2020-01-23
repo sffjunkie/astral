00f6ea89fd072a41274b4cf8730256017b7b1808
Simon Kennedy <sffjunkie+code@gmail.com>
2020-01-23 12:25:59 +0000

    Renamed test database fixture

--- src/test/test_buenos_aries.py
+++ src/test/test_buenos_aries.py
@@ -2,6 +2,6 @@
 from astral.geocoder import lookup
 
 
-def test_BuenosAries(astral_database):
-    b = lookup("Buenos Aires", astral_database)
+def test_BuenosAries(test_database):
+    b = lookup("Buenos Aires", test_database)
     assert b.timezone == "America/Buenos_Aires"
--- src/test/test_geocoder.py
+++ src/test/test_geocoder.py
@@ -11,17 +11,17 @@ def location_count(name, locations):
 class TestDatabase:
     """Test database access functions"""
 
-    def test_all_locations(self, astral_database):
-        for loc in astral.geocoder.all_locations(astral_database):
+    def test_all_locations(self, test_database):
+        for loc in astral.geocoder.all_locations(test_database):
             assert loc.name
 
-        location_list = astral.geocoder.all_locations(astral_database)
+        location_list = astral.geocoder.all_locations(test_database)
         all_locations = list(location_list)
         assert location_count("London", all_locations) == 1
         assert location_count("Abu Dhabi", all_locations) == 2
 
-    def test_lookup(self, astral_database):
-        loc = astral.geocoder.lookup("London", astral_database)
+    def test_lookup(self, test_database):
+        loc = astral.geocoder.lookup("London", test_database)
         assert loc.name == "London"
         assert loc.region == "England"
         assert approx(loc.latitude, 51.5)
@@ -31,77 +31,77 @@ class TestDatabase:
         assert tz == tzl
         assert loc.elevation == 24
 
-    def test_city_in_db(self, astral_database):
-        astral.geocoder.lookup("london", astral_database)
+    def test_city_in_db(self, test_database):
+        astral.geocoder.lookup("london", test_database)
 
-    def test_group_in_db(self, astral_database):
-        astral.geocoder.lookup("africa", astral_database)
+    def test_group_in_db(self, test_database):
+        astral.geocoder.lookup("africa", test_database)
 
-    def test_location_not_in_db(self, astral_database):
+    def test_location_not_in_db(self, test_database):
         with raises(KeyError):
-            astral.geocoder.lookup("Nowhere", astral_database)
+            astral.geocoder.lookup("Nowhere", test_database)
 
-    def test_group_not_in_db(self, astral_database):
+    def test_group_not_in_db(self, test_database):
         with raises(KeyError):
-            astral.geocoder.group("wallyland", astral_database)
+            astral.geocoder.group("wallyland", test_database)
 
-    def test_lookup_city_and_region(self, astral_database):
+    def test_lookup_city_and_region(self, test_database):
         city_name = "Birmingham,England"
 
-        city = astral.geocoder.lookup(city_name, astral_database)
+        city = astral.geocoder.lookup(city_name, test_database)
         assert city.name == "Birmingham"
         assert city.region == "England"
 
-    def test_country_with_multiple_entries_no_country(self, astral_database):
-        city = astral.geocoder.lookup("Abu Dhabi", astral_database)
+    def test_country_with_multiple_entries_no_country(self, test_database):
+        city = astral.geocoder.lookup("Abu Dhabi", test_database)
         assert city.name == "Abu Dhabi"
 
-    def test_country_with_multiple_entries_with_country(self, astral_database):
+    def test_country_with_multiple_entries_with_country(self, test_database):
         """Test for fix made due to bug report from Klaus Alexander Seistrup"""
 
-        city = astral.geocoder.lookup("Abu Dhabi,United Arab Emirates", astral_database)
+        city = astral.geocoder.lookup("Abu Dhabi,United Arab Emirates", test_database)
         assert city.name == "Abu Dhabi"
 
-        city = astral.geocoder.lookup("Abu Dhabi,UAE", astral_database)
+        city = astral.geocoder.lookup("Abu Dhabi,UAE", test_database)
         assert city.name == "Abu Dhabi"
 
 
 class TestBugReports:
     """Test for bug report fixes"""
 
-    def test_Adelaide(self, astral_database):
+    def test_Adelaide(self, test_database):
         """Test for fix made due to bug report from Klaus Alexander Seistrup"""
 
-        astral.geocoder.lookup("Adelaide", astral_database)
+        astral.geocoder.lookup("Adelaide", test_database)
 
-    def test_CandianCities(self, astral_database):
-        city = astral.geocoder.lookup("Fredericton", astral_database)
+    def test_CandianCities(self, test_database):
+        city = astral.geocoder.lookup("Fredericton", test_database)
         assert city.elevation == 8
 
 
 class TestDatabaseAddLocations:
     """Test adding locations to database"""
 
-    def test_newline_at_end(self, astral_database):
-        count = astral.geocoder._location_count(astral_database)
+    def test_newline_at_end(self, test_database):
+        count = astral.geocoder._location_count(test_database)
         astral.geocoder.add_locations(
-            "A Place,A Region,Asia/Nicosia,35°10'N,33°25'E,162.0\n", astral_database
+            "A Place,A Region,Asia/Nicosia,35°10'N,33°25'E,162.0\n", test_database
         )
-        assert astral.geocoder._location_count(astral_database) == count + 1
+        assert astral.geocoder._location_count(test_database) == count + 1
 
-    def test_from_list_of_strings(self, astral_database):
-        count = astral.geocoder._location_count(astral_database)
+    def test_from_list_of_strings(self, test_database):
+        count = astral.geocoder._location_count(test_database)
         astral.geocoder.add_locations(
             [
                 "A Place,A Region,Asia/Nicosia,35°10'N,33°25'E,162.0",
                 "Another Place,Somewhere else,Asia/Nicosia,35°10'N,33°25'E,162.0",
             ],
-            astral_database,
+            test_database,
         )
-        assert astral.geocoder._location_count(astral_database) == count + 2
+        assert astral.geocoder._location_count(test_database) == count + 2
 
-    def test_from_list_of_lists(self, astral_database):
-        count = astral.geocoder._location_count(astral_database)
+    def test_from_list_of_lists(self, test_database):
+        count = astral.geocoder._location_count(test_database)
         astral.geocoder.add_locations(
             [
                 ["A Place", "A Region", "Asia/Nicosia", "35°10'N", "33°25'E", "162.0"],
@@ -114,9 +114,9 @@ class TestDatabaseAddLocations:
                     "162.0",
                 ],
             ],
-            astral_database,
+            test_database,
         )
-        assert astral.geocoder._location_count(astral_database) == count + 2
+        assert astral.geocoder._location_count(test_database) == count + 2
 
 
 def test_SanitizeKey():
