9e81d233d47d3a4f3d5cd3831630aa33ec3ec0e5
Simon Kennedy <sffjunkie+code@gmail.com>
2020-01-28 11:01:48 +0000

    Corrected logic

--- src/test/almost_equal.py
+++ src/test/almost_equal.py
@@ -2,14 +2,15 @@ from datetime import datetime
 
 import pytz
 
-def datetime_almost_equal(datetime1:datetime, datetime2:datetime, seconds:int=60):
-    if not(datetime1.tzinfo):
+    if not (datetime1.tzinfo):
         datetime1 = pytz.utc.localize(datetime1)
-    datetime1 = datetime1.astimezone(pytz.utc)
+    else:
+        datetime1 = datetime1.astimezone(pytz.utc)
 
-    if not(datetime2.tzinfo):
+    if not (datetime2.tzinfo):
         datetime2 = pytz.utc.localize(datetime2)
-    datetime2 = datetime2.astimezone(pytz.utc)
+    else:
+        datetime2 = datetime2.astimezone(pytz.utc)
 
     dd = datetime1 - datetime2
     sd = (dd.days * 24 * 60 * 60) + dd.seconds
