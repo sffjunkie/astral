2c6cf97b136cfe2195569f60419c19a5d1e5105c
Simon Kennedy <sffjunkie+code@gmail.com>
2022-10-05 19:03:44 +0100

    Added typing info

--- src/test/test_all.py
+++ src/test/test_all.py
@@ -1,6 +1,7 @@
-from astral.geocoder import all_locations
+from astral.geocoder import all_locations, LocationDatabase
 from astral.sun import noon
 
-def test_AllLocations(test_database):
+
+def test_AllLocations(test_database: LocationDatabase):
     for location in all_locations(test_database):
         noon(location.observer)
--- src/test/test_buenos_aries.py
+++ src/test/test_buenos_aries.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from astral.geocoder import lookup
+from astral.location import LocationInfo
+from astral.geocoder import lookup, LocationDatabase
 
 
-def test_BuenosAries(test_database):
+def test_BuenosAries(test_database: LocationDatabase):
     b = lookup("Buenos Aires", test_database)
+    assert isinstance(b, LocationInfo)
     assert b.timezone == "America/Buenos_Aires"
