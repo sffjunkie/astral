1780fb5d8f2b321b3273789dbd914fc885210584
Simon Kennedy <sffjunkie+code@gmail.com>
2022-10-05 19:04:14 +0100

    Ignore type errors

--- src/test/test_depression_not_reached.py
+++ src/test/test_depression_not_reached.py
@@ -10,7 +10,13 @@ def test_Dawn_NeverReachesDepression():
     d = datetime.date(2016, 5, 29)
     with pytest.raises(ValueError):
         loc = Location(
-            LocationInfo("Ghent", "Belgium", "Europe/Brussels", "51°3'N", "3°44'W")
+            LocationInfo(
+                "Ghent",
+                "Belgium",
+                "Europe/Brussels",
+                "51°3'N",
+                "3°44'W",
+            )  # type: ignore
         )
         loc.solar_depression = 18
         loc.dawn(date=d, local=True)
