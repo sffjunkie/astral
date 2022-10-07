e18e90cd63a9a49342a43a45355f372c71b391f1
Simon Kennedy <sffjunkie+code@gmail.com>
2022-10-07 20:21:18 +0100

    Deleted mock as we don't use pytz any more.

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -4,19 +4,6 @@ import sys
 
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
-# http://read-the-docs.readthedocs.org/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
-autodoc_mock_imports = ["pytz"]
-# MOCK_MODULES = ["pytz"]
-# if MOCK_MODULES and on_rtd:
-#     from unittest.mock import MagicMock
-
-#     class Mock(MagicMock):
-#         @classmethod
-#         def __getattr__(cls, name):
-#             return Mock()
-
-#     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
 # region General configuration ----------------------------------------------------
 
 project = "Astral"
