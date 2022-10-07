dfd430a14853ad0e47a2b2fe98ba72cd29196d07
Simon Kennedy <sffjunkie+code@gmail.com>
2022-10-07 09:57:22 +0100

    Added missing date to test

--- src/test/test_sun_calc.py
+++ src/test/test_sun_calc.py
@@ -191,6 +191,7 @@ def test_Elevation(new_delhi: Location):
 
 
 def test_Elevation_NonNaive(new_delhi: Location):
+    d = datetime.datetime(2001, 6, 21, 18, 41, 0, tzinfo=new_delhi.tz)
     assert sun.elevation(new_delhi.observer, d) == pytest.approx(7.41, abs=0.1)  # type: ignore
 
 
