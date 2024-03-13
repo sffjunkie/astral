92ed0a72be7f2af446e1da60692a96fa96761ac6
Simon Kennedy <sffjunkie+code@gmail.com>
2024-03-13 19:12:13 +0000

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Astral - Calculations for the sun and moon.";
+
+  inputs.pyproject-nix.url = "github:nix-community/pyproject.nix";
+  inputs.pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+  nixConfig = {
+    bash-prompt = ''\n\[\033[1;34m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] '';
+  };
+
+  outputs = {
+    nixpkgs,
+    pyproject-nix,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+
+    project = pyproject-nix.lib.project.loadPyproject {
+      projectRoot = ./.;
+    };
+
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (pkgs: (
+      let
+        python = pkgs.python3;
+        arg = project.renderers.withPackages {inherit python;};
+        pythonEnv = python.withPackages arg;
+      in {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.pdm
+            pythonEnv
+          ];
+        };
+      }
+    ));
+
+    packages = forAllSystems (pkgs: (
+      let
+        python = pkgs.python3;
+        attrs = project.renderers.buildPythonPackage {inherit python;};
+      in {
+        default = python.pkgs.buildPythonPackage attrs;
+      }
+    ));
+  };
+}
